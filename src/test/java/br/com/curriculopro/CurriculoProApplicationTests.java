540897aa676dec87834ecae3ab895a4a4f8dd5ec
EdilsonFranca <edilson18martins@gmail.com>
2026-07-05 23:49:53 -0300

    Merge remote-tracking branch 'origin/main'

--- src/test/java/br/com/curriculopro/CurriculoProApplicationTests.java
+++ src/test/java/br/com/curriculopro/CurriculoProApplicationTests.java
@@ -3,8 +3,8 @@ package br.com.curriculopro;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ResumeApplicationTests {
+@SpringBootTest(classes = CurriculoProApplication.class)
+class CurriculoProApplicationTests {
 
 	@Test
 	void contextLoads() {
